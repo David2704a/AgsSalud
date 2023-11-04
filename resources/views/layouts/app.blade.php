7f50fc9dc62bca0b051d2de7e5959cdb5ee55e5a
Johan <davidbm270@gmail.com>
2023-11-03 21:58:53 -0500

    Creacion de los estilos de las tablas y la vista de Procedimiento

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('/css/LayoutFondo.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/LayoutApp.css')}}">
+    <link rel="shortcut icon" href="{{asset('imgs/logos/Ags.png')}}" type="image/x-icon">
     <title>@yield('title')</title>
 </head>
 <body>
 
-    
+
     <header>
         <div class="logo">
             <img src="{{asset('imgs/logos/Ags.png')}}" alt="Logo de la empresa">
@@ -30,8 +31,8 @@
         @yield('content')
         @yield('links')
      </div>
-    
 
- 
+
+
 </body>
 </html>
--- resources/views/procedimientos/procedimiento/index.blade.php
+++ resources/views/procedimientos/procedimiento/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('title', 'Procedimiento')
-    
+
 
 @section('links')
-    
+
 <link rel="stylesheet" href="{{asset('/css/procedimiento/procedimiento.css')}}">
 
 
@@ -15,10 +15,100 @@
 <div class="content">
     <h1 class="page-title">Procedimientos</h1>
     <div class="green-line"></div>
+
+    <div class="menu-container">
+        <ul class="menu">
+            <li>
+                <a href="#">En proceso</a>
+                <div class="linea"></div>
+            </li>
+            <li>
+                <a href="#">Terminados</a>
+                <div class="linea"></div>
+
+            </li>
+            <li>
+                <a href="#">Pendientes</a>
+                <div class="linea active"></div>
+            </li>
+        </ul>
+    </div>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                   <th>ID</th>
+                   <th>Fecha Inicio</th>
+                   <th>Fecha Fin</th>
+                   <th>Hora</th>
+                   <th>Fecha Reprogramada</th>
+                   <th>Observación</th>
+                   <th>Responsable de Entrega</th>
+                   <th>Responsable que Recibe</th>
+                   <th>Elemento</th>
+                   <th>Estado del Procedimiento</th>
+                   <th>Tipo de Procedimiento</th>
+                   <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($procedimiento as $procedimiento)
+            <tr>
+                <td>
+                    {{$procedimiento->idProcedimiento}}
+                </td>
+                <td>
+                    {{$procedimiento->fechaInicio ? $procedimiento->fechaInicio : 'No aplica'}}
+                </td>
+                <td>
+                    {{$procedimiento->fechaFin ? $procedimiento->fechaFin : 'No aplica'}}
+                </td>
+                <td>
+                    {{$procedimiento->hora ? $procedimiento->hora : 'No aplica'}}
+                </td>
+                <td>
+                    {{$procedimiento->fechaReprogramada ? $procedimiento->fechaReprogramada : 'No aplica'}}
+                </td>
+                <td>
+                    {{$procedimiento->observacion}}
+                </td>
+                <td>
+                    {{$procedimiento->idResponsableEntrega ? $procedimiento->idResponsableEntrega : 'No aplica'}}
+                </td>
+                <td>
+                    {{$procedimiento->idResponsableRecibe ? $procedimiento->idResponsableRecibe : 'No aplica'}}
+                </td>
+                <td>
+                    {{$procedimiento->elemento->modelo }}
+                </td>
+                <td>
+                    {{$procedimiento->estadoProcedimiento->estado}}
+                </td>
+                <td>
+                    {{$procedimiento->tipoProcedimiento->tipo}}
+                </td>
+                <td>
+                    <a href="{{route('editProcedimiento', $procedimiento->idProcedimiento)}}">Editar</a>
+
+                    <form action="{{route('destroyProcedimiento', ['id' => $procedimiento->idProcedimiento])}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Eliminar</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
 </div>
 
 
-    <a href="{{route('createProcedimiento')}}">Crear</a>
+
+
+    {{-- <a href="{{route('createProcedimiento')}}">Crear</a>
     <a href="{{route('createEstadoP')}}">Crear estadoP</a>
     <a href="{{route('createTipoP')}}">Crear tipoP</a>
 
@@ -108,6 +198,6 @@
             @endforeach
 
         </tbody>
-    </table>
+    </table> --}}
 
 @endsection
