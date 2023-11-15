010d654687e40e60d8b5640b7c6971b441b480af
vanesa <vanesagalindez318@gmail.com>
2023-11-15 17:05:43 -0500

    login estilos listos funcionando todo

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,46 @@
-<x-guest-layout>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="shortcut icon" href="img/Logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('inicio/css/forgot-password.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+
+
+</head>
+
+<body class="hold-transition login-page">
+
+</body>
+<div class="contenedortodo">
+
+<div class="vansss">
+
+            <img src="{{ asset('img/escudo.png') }}" style="width: 70%; height: 90%">
+            
+            <img src="{{ asset('img/fondoemprender.png') }}" style="width: 70%; height: 90%">
+
+            <img src="{{ asset('img/sena.png') }}" style="width: 70%; height: 90%">
+            <img src="{{ asset('img/IQNet.png') }}" style="width: 70%; height: 90%">
+            <img src="{{ asset('img/enterritorio.png') }}" style="width: 70%; height: 90%">
+
+            <img  class ="peque" src="{{ asset('img/icontec.png') }}" style="width: 70%; height: 90%">
+
+           
+</div>
+<br>
+<br>
+
+    <div class="container  w-full sm:max-w-md mt-6 px-6 py-4  shadow-md overflow-hidden sm:rounded-lg" >
+
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('¿Olvidaste tu contraseña? Ningún problema. Simplemente háganos saber su dirección de correo electrónico y le enviaremos un enlace para restablecer su contraseña que le permitirá elegir una nueva...') }}
     </div>
@@ -16,10 +58,22 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+        <div class="btn2 items-center  ">
+            <x-primary-button class="btn ms-3">
                 {{ __('Enlace para restablecer contraseña de correo electrónico') }}
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+    <br>
+<br>
+        
+        </div>
+        <div class="vansss2">
+        <img src="{{ asset('img/sinfondologo.png') }}" style="width: 70%; height: 90%">
+
+        </div>
+    </div>
+    </div>
+
+
+
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,122 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Correo electronico')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <title>{{"Vansss"}}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('fontawesome-free/css/all.min.css') }}">
+
+    <link  rel="icon" href="{{ asset('img/sinfondologo.png') }}" type="image/png" />
+
+    <!-- Styles -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+    <link href="{{ asset('inicio/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('inicio/css/estilos.css') }}" rel="stylesheet">
+
+
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="row">
+            <!-- Sesion izquierda de la pagina-->
+            <div class="izqui col-lg-5 col-md-8 mx-auto">
+                <img class="img-fluid ags" src="{{ asset('img/AgsLogin.png') }}" alt="">
+                <img class="img-fluid Grupo_1" src="{{ asset('img/GrupoImagenes.png')}}" alt="">
+            </div>
+
+            <!-- Linea central de la pagina-->
+            <div class="linea1"></div>
+
+            <!-- Sesion derecha de la pagina-->
+            <div class="dere col-lg-5 col-md-8 mx-auto">
+                <img class="img-fluid users" src="{{ asset('img/Aly.png')}}" alt="">
+                <div class="mt-4">
+                    @if (Session::has('mensaje'))
+                        <div class="alert alert-danger" role="alert">
+                            {{Session::get('mensaje')}}
+                        </div>
+                    @endif
+                </div>
+
+                <div class="login">
+                    <!-- Formulario de iniciar sesion -->
+                    <form action="{{ url('/login') }}" class="form-login" method="post">
+                        @csrf
+                        <div class="input-group flex-nowrap mt-4 col-lg-8 col-md-12 col-sm-12 mx-auto">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="addon-wrapping"><img class="loginimg" src="{{ asset('img/Icon_feather_user.png') }}" alt=""></span>
+                            </div>
+                            <input id="email" name="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="E-MAIL" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        </div>
+                        <div class="col-lg-8 col-md-12 col-sm-12 mx-auto">
+                            @error('email')
+                                <div class="alert alert-danger"   style="position: relative;" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="input-group flex-nowrap mt-4 col-lg-8 col-md-12 col-sm-12 mx-auto">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="addon-wrapping"><img class="loginimg" src="{{ asset('img/Icon_feather_lock.png ') }}" alt=""></span>
+                            </div>
+                            <input id="password" name="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="CONTRASEÑA" value="{{ old('password') }}" required autocomplete="current-password">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="addon-wrapping"><a onclick="mostrarContrasena()" onchange="cambiarOjo()"><i id="the-eye" class="fa fa-eye-slash"></i></a></span>
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-12 col-sm-12 mx-auto">
+                            @error('password')
+                                <div class="alert alert-danger" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block mt-4 col-lg-7 col-md-11 col-11 mx-auto">INICIAR SESION</button>
+                    </form>
+                    <!-- Final del formulario de iniciar sesion -->
+                </div>
+            </div>
         </div>
+        <br>
+        <br>
+        <br><br>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Contraseña')" />
+    <!-- Pie de pagina -->
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    <footer style="background-color: #EBEBEB">
+        <div class="mt-3 d-flex flex-wrap justify-content-around">
+            <div class="mx-auto">
+                <hr>
+                <div class="input-group">
+                    <span class="small" style="color: gray">
+                        <b>Copyright &copy; 2023 AGS SALUD SAS <br> Todos los derechos reservados</b>
+                        {{-- <div class="lt text-gray">Vansss = Jessica Vanesa Acosta Galindez   <br> DESARROLLADORA</div> --}}
+                    </span>
+                </div>
+            </div>
         </div>
+        <br>
+    </footer>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recuerdame') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Olvidaste tu contraseña?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Iniciar') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</body>
+<script src="{{ asset('inicio/js/elojito.js') }}"></script>
+<script src="{{ asset('inicio/js/jquery.knob.min.js') }}" defer type="text/javascript"></script>
+<script src="{{ asset('inicio/js/jquery.imagecursorzoom.js') }}" defer type="text/javascript"></script>
+</html>
