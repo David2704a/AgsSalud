7b04cbf9fa230e0996649e6d66d0a15b90656683
vanesa <vanesagalindez318@gmail.com>
2023-11-24 13:24:20 -0500

    vansss: arregle un poco la vista de perfil solucione errores y hice merge con vansss

--- resources/views/persona/index.blade.php
+++ resources/views/persona/index.blade.php
@@ -1,17 +1,38 @@
+@extends('layouts.app')
 
+@section('title', 'Perfil')
 
-
-@section('title', 'EditarPerfil')
-
-@php
+@php 
     use Illuminate\Support\Facades\Auth;
 @endphp
 
 
-    <h1 style="margin-left:40%"><span class="pf pf-atm">Actualizar Perfil</span></h1>
+
+@section('links')
+
+<link rel="stylesheet" href="{{asset('/css/categoria/categoria.css')}}">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+@endsection
 
 
 
+@section('content')
+<div class="content">
+<h1 class="page-title">Actualizar perfil</h1>
+<div class="green-line"></div>
+
+<div class="button-container">
+    <a href="/dashboard" class="button-izquierda arrow-left"><i class="fa-solid fa-circle-arrow-left"></i> Regresar</a>
+</div>
+
+
+
+   
+    <div class="table-container">
+        
+          
     <div class="card" style="width:50%; margin-left:25%">
         <div class="card-body">
             <form action="{{ route('Actualizar') }}" method="POST" role="form" enctype="multipart/form-data">
@@ -37,11 +58,65 @@
                             value="{{ Auth::user()->email}}" class="form-control">
                     </div>
                 </div>
+                <br>
+                <br>
                 <div class="row text-center mb-4 mt-5">
                     <div class="cold-md-12">
                         <button type="submit" class=" btn btn-danger" id="formSubmit">Guardar cambios</button>
                         <a href="{{ route('editarPerfil', ['id' => Auth::user()->id]) }}" class="btn btn-primary">Actualizar Información</a>
                 </div>
             </form>
+
+            <br>
+<br>
+<br>
         </div>
     </div>
+  
+    </div>
+
+
+
+
+</div>
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+
+
+
+
+<footer class="footer">
+    <div class="left-images">
+        <div class="column">
+            <img src="{{ asset('imgs/logos/logo-sena.png') }}" width="45" alt="Imagen 1">
+            <img src="{{ asset('imgs/logos/ESCUDO COLOMBIA.png') }}" width="45" alt="Imagen 2">
+        </div>
+        <div class="column">
+            <img src="{{ asset('imgs/logos/logo_fondo.png') }}" width="130" alt="Imagen 3">
+            <img src="{{ asset('imgs/logos/Logo_Enterritorio.png') }}" width="100" alt="Imagen 4">
+        </div>
+    </div>
+    <div class="right-content">
+        <div class="images">
+            <img src="{{ asset('imgs/logos/LOGO ISO.png') }}" width="50" alt="Imagen 5">
+            <img src="{{ asset('imgs/logos/Logo-IQNet.png') }}" width="75" alt="Imagen 6">
+        </div>
+        <div class="separator"></div>
+        <div class="text">
+            <p>Copyright © 2023 AGS SALUD SAS</p>
+            <p>Todos los derechos Reservados</p>
+        </div>
+    </div>
+</footer>
+@endsection
