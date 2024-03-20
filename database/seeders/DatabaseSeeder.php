4bd1b1272d30a0d22c1a03239d6138d765bee3f0
Johan David Bolaños Muñoz <davidbm270@gmail.com>
2024-03-20 11:50:13 -0500

    Arreglo en el modulo de procedimientos2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleSeeder::class);
         $this->call(EstadoProcedimientoSeeder::class);
-        // $this->call(TipoProcedimientoSeeder::class);
+        $this->call(TipoProcedimientoSeeder::class);
         // $this->call(EstadoElementoSeeder::class);
         // $this->call(TipoElementoSeeder::class);
         // $this->call(CategoriaSeeder::class);
