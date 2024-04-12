f3329d6fb87ea078bfdb1ef6def0629507bfaea4
Johan David Bolaños Muñoz <davidbm270@gmail.com>
2024-04-12 15:57:26 -0500

    Arreglo de footer en todos los modulos

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('content')
-    <div class="content">
+    <div class="content2">
         <h1 class="page-title">Usuarios</h1>
         <div class="green-line"></div>
 
@@ -128,31 +128,4 @@
         </div>
     </div>
 
-    <br>
-    <br>
-    <br>
-
-    <footer class="footer">
-        <div class="left-images">
-            <div class="column">
-                <img src="{{ asset('imgs/logos/logo-sena.png') }}" width="45" alt="Imagen 1">
-                <img src="{{ asset('imgs/logos/ESCUDO COLOMBIA.png') }}" width="45" alt="Imagen 2">
-            </div>
-            <div class="column">
-                <img src="{{ asset('imgs/logos/logo_fondo.png') }}" width="130" alt="Imagen 3">
-                <img src="{{ asset('imgs/logos/Logo_Enterritorio.png') }}" width="100" alt="Imagen 4">
-            </div>
-        </div>
-        <div class="right-content">
-            <div class="images">
-                {{-- <img src="{{ asset('imgs/logos/LOGO ISO.png') }}" width="50" alt="Imagen 5"> --}}
-                {{-- <img src="{{ asset('imgs/logos/Logo-IQNet.png') }}" width="75" alt="Imagen 6"> --}}
-            </div>
-            <div class="separator"></div>
-            <div class="text">
-                <p>Copyright © 2024 AGS SALUD SAS</p>
-                <p>Todos los derechos Reservados</p>
-            </div>
-        </div>
-    </footer>
 @endsection
