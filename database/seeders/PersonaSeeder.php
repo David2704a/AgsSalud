18c7ebff3763260ab3428a9aa000a465e48d9a9b
Vansss <vanesagalindez318@gmail.com>
2024-05-08 14:34:26 -0500

    listo 3

--- database/seeders/PersonaSeeder.php
+++ database/seeders/PersonaSeeder.php
@@ -13,50 +13,7 @@ class PersonaSeeder extends Seeder
      */
     public function run(): void
     {
-        Persona::create([
-            'nombre1' => 'Juan',
-            'apellido1' => 'Pérez',
-            'idTipoIdentificacion' => 1, // Reemplaza con el ID correcto del tipo de identificación
-            'identificacion' => '12345789',
-            'fechaNac' => '1990-01-01',
-            'sexo' => 'M',
-            'direccion' => 'Calle 123',
-            // 'email' => 'juan@example.com',
-            'celular' => '1234567890',
-        ]);
-        Persona::create([
-            'nombre1' => 'Andres',
-            'apellido1' => 'Galindez',
-            'idTipoIdentificacion' => 1, // Reemplaza con el ID correcto del tipo de identificación
-            'identificacion' => '987654321',
-            'fechaNac' => '1990-01-01',
-            'sexo' => 'M',
-            'direccion' => 'Calle 123',
-            // 'email' => 'juan@example.com',
-            'celular' => '1234567890',
-        ]);
-        Persona::create([
-            'nombre1' => 'Juan',
-            'apellido1' => 'Orozco',
-            'idTipoIdentificacion' => 1, // Reemplaza con el ID correcto del tipo de identificación
-            'identificacion' => '09876567329',
-            'fechaNac' => '1990-01-01',
-            'sexo' => 'M',
-            'direccion' => 'Calle 123',
-            // 'email' => 'Juan@gmail.com',
-            'celular' => '1234567890',
-        ]);
-        Persona::create([
-            'nombre1' => 'Johan',
-            'apellido1' => 'Bolaños',
-            'idTipoIdentificacion' => 1, // Reemplaza con el ID correcto del tipo de identificación
-            'identificacion' => '1234567893',
-            'fechaNac' => '1990-01-01',
-            'sexo' => 'M',
-            'direccion' => 'Calle 123',
-            // 'email' => 'johan@gmail.com',
-            'celular' => '1234567890',
-        ]);
+       
 
 
 }
